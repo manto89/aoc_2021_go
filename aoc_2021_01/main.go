67f9391873211976e3a78ca9c9012ea44b1f23ce
manto89 <manto89@gmail.com>
2021-12-14 17:59:58 +0100

    Finished day01p2 with refactoring of day01p1

--- aoc_2021_01/main.go
+++ aoc_2021_01/main.go
@@ -1,13 +1,14 @@
 package aoc_2021_01
 
 import (
+	"errors"
 	"github.com/manto89/aoc_2021_go/utils"
 	"log"
 )
 
 var fileName = "./input/01"
 
-func readLines() {
+func getMeasurementsReport(windowSize int) {
 	ints, err := utils.ReadInts(fileName)
 	if err != nil {
 		log.Fatal(err)
@@ -15,23 +16,51 @@ func readLines() {
 	if len(ints) < 1 {
 		log.Fatalf("File doesn't contain any number")
 	}
-	lastInt := ints[0]
+	lastMeasurements := 0
 	increasingCounter := 0
 	decreasingCounter := 0
-	for i := 1; i < len(ints); i++ {
-		if ints[i] > lastInt {
-			increasingCounter++
-		} else {
-			decreasingCounter++
-		}
-		lastInt = ints[i]
+	for i := 0; i < len(ints); i++ {
+		func() {
+			defer func() {
+				lastMeasurements, _ = getWindowSum(ints, i, windowSize)
+			}()
+			if i < 1 {
+				return
+			}
+			newInt, err := getWindowSum(ints, i, windowSize)
+			if err != nil {
+				return
+			}
+			if newInt > lastMeasurements {
+				increasingCounter++
+			} else {
+				decreasingCounter++
+			}
+		}()
 	}
 	log.Printf("Increasing %d, Decreasing %d", increasingCounter, decreasingCounter)
 }
+func getWindowSum(ints []int, counter int, windowSize int) (int, error) {
+	var ret = 0
+	if counter+windowSize > len(ints) {
+		return 0, errors.New("Not enough measurements")
+	}
+	for i := 0; i < windowSize; i++ {
+		ret += ints[counter+i]
+	}
+	return ret, nil
+}
 
 func Solve() {
-	log.Printf("**** DAY 01 ****")
+	log.Printf("**** DAY 01 Part01****")
 	log.Printf("Given a list of number, count how many numbers are bigger than the previous one")
-	readLines()
+	getMeasurementsReport(1)
+	log.Printf("****")
+}
+
+func SolvePart2() {
+	log.Printf("**** DAY 01 Part02 ****")
+	log.Printf("Given a list of number, count how many numbers are bigger than the previous one using a three-measurement sliding window")
+	getMeasurementsReport(3)
 	log.Printf("****")
 }
--- main.go
+++ main.go
@@ -5,4 +5,5 @@ import ex01 "github.com/manto89/aoc_2021_go/aoc_2021_01"
 func main() {
 
 	ex01.Solve()
+	ex01.SolvePart2()
 }
